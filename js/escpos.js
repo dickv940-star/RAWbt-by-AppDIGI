6de5c80fb597abd777827b17f1532984e03d0235
dickv940-star <dickv940@gmail.com>
2026-06-29 19:08:10 +0700

    Refactor textPrint function and add ESC commands

--- js/escpos.js
+++ js/escpos.js
@@ -1,11 +1,44 @@
-function textPrint(text){
+function initESC(){
+
+
+return new Uint8Array([
+
+0x1B,0x40
+
+]);
+
+}
+
+
+
+
+function cutPaper(){
+
+
+return new Uint8Array([
+
+0x1D,
+0x56,
+0x00
+
+]);
+
+
+}
+
+
+
+
+function printText(txt){
+
 
 let encoder =
 new TextEncoder();
 
 
 return encoder.encode(
-text+"\n\n"
+txt+"\n\n"
 );
+
 
 }
