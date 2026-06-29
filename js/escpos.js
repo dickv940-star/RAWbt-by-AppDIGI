a3908290881ebb80443a42220e943913b5b3e9fb
dickv940-star <dickv940@gmail.com>
2026-06-29 19:13:00 +0700

    Update escpos.js

--- js/escpos.js
+++ js/escpos.js
@@ -1,44 +1,33 @@
 function initESC(){
 
-
 return new Uint8Array([
-
-0x1B,0x40
-
+0x1B,
+0x40
 ]);
 
 }
-
 
 
 
 function cutPaper(){
 
-
 return new Uint8Array([
-
 0x1D,
 0x56,
 0x00
-
 ]);
-
 
 }
 
 
-
-
-function printText(txt){
-
+function printText(text){
 
 let encoder =
 new TextEncoder();
 
 
 return encoder.encode(
-txt+"\n\n"
+text+"\n\n"
 );
-
 
 }
