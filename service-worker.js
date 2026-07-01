65c9377e050e029b0dc5d81679842c3b065af0c0
dickv940-star <dickv940@gmail.com>
2026-07-01 16:47:41 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,111 +1,63 @@
-const CACHE_NAME = "rawbt-v2";
+const CACHE_NAME = "rawbt-v3";
 
-
-const FILES=[
-
-"./",
-"./index.html",
-"./manifest.json",
-"./icon.png"
-
+const FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon.png",
+    "./icon-192.png",
+    "./icon-512.png",
+    "./css/style.css",
+    "./js/bluetooth.js",
+    "./js/escpos.js",
+    "./js/thermal.js"
 ];
 
-
-
 // INSTALL
+self.addEventListener("install", event => {
+    self.skipWaiting();
 
-self.addEventListener(
-"install",
-event=>{
-
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache=>{
-
-return cache.addAll(FILES);
-
-})
-
-);
-
-
-self.skipWaiting();
-
-
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
 });
 
+// ACTIVATE
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
 
-
-
-
-// AKTIF
-
-
-self.addEventListener(
-"activate",
-event=>{
-
-
-event.waitUntil(
-
-caches.keys()
-.then(keys=>{
-
-
-return Promise.all(
-
-keys.map(key=>{
-
-
-if(key!==CACHE_NAME)
-
-return caches.delete(key);
-
-
-})
-
-);
-
-
-})
-
-
-);
-
-
+    self.clients.claim();
 });
 
+// FETCH
+self.addEventListener("fetch", event => {
 
+    event.respondWith(
 
+        fetch(event.request)
+            .then(response => {
 
+                const clone = response.clone();
 
+                caches.open(CACHE_NAME)
+                    .then(cache => cache.put(event.request, clone));
 
-// OFFLINE MODE
+                return response;
 
+            })
+            .catch(() => caches.match(event.request))
 
-self.addEventListener(
-"fetch",
-event=>{
-
-
-event.respondWith(
-
-
-caches.match(event.request)
-
-.then(response=>{
-
-
-return response ||
-fetch(event.request);
-
-
-})
-
-
-);
-
+    );
 
 });
